3425f64c52fe1bb30f8f4e124969e6bffedb3db2
ErckLima <erickfalubay@gmail.com>
2022-10-18 21:09:08 -0300

    Ajustando menu

---
+++ scripts.js
@@ -0,0 +1,12 @@
+function clickMenu(){
+    var menu = document.getElementById("Menu")
+    if(menu.style.display == 'block'){
+        menu.style.display = 'none'
+    }
+    else{
+        menu.style.display = 'block'
+    }
+    
+
+    
+}
