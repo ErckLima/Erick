50f0723f3d4d81226c0be9294e254570a2068648
Erick Lima <110945352+ErckLima@users.noreply.github.com>
2022-10-19 13:58:05 -0300

    Menu retirado

--- scripts.js
+++
@@ -1,9 +0,0 @@
-function clickMenu(){
-    var menu = document.getElementById("Menu")
-    if(menu.style.display == 'block'){
-        menu.style.display = 'none'
-    }
-    else{
-        menu.style.display = 'block'
-    } 
-}
